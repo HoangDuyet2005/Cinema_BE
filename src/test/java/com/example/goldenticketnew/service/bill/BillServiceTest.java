f03cd043e6d76a9991c81ca3ccae937de3874230
HoangDuyet2005 <theduyethoang0105@gmail.com>
2026-08-24 13:49:25 +0700

    chore(test): clean up unused imports in BillServiceTest and MovieServiceTest

--- src/test/java/com/example/goldenticketnew/service/bill/BillServiceTest.java
+++ src/test/java/com/example/goldenticketnew/service/bill/BillServiceTest.java
@@ -22,7 +22,6 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
